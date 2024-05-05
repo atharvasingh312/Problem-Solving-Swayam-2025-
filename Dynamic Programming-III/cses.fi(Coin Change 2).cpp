7e60c193847fd4ee65368a167a6772b7da295c4a
atharvasingh312 <111992277+atharvasingh312@users.noreply.github.com>
2024-05-05 23:44:31 +0530

    Create cses.fi(Coin Change 2).cpp

---
+++ Dynamic Programming-III/cses.fi(Coin Change 2).cpp
@@ -0,0 +1,43 @@
+//https://cses.fi/problemset/view/1636/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+//recursion and memoization:
+/*long long solve(int i,int x,vector<int> &coins,vector<vector<int>> &dp){
+    int mod=(int)(1e9+7);
+    if(x==0) return 1;
+    else if(i==(int)coins.size()) return 0;
+    else if(dp[i][x]!=-1) return dp[i][x];
+    long long p=0;
+    if(x-coins[i]>=0){
+        p=(p+solve(i,x-coins[i],coins,dp))%mod;
+    }
+    return dp[i][x]=(p+solve(i+1,x,coins,dp))%mod;
+}*/
+int main()
+{
+    int n,x;
+    cin>>n>>x;
+    vector<int> coins(n,0);
+    vector<vector<int>> dp(n+1,vector<int>(x+1,0));
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    int mod=(int)(1e9+7);
+    //int ans=solve(0,x,coins,dp);
+    //cout<<ans<<endl;
+    for(int i=0;i<=n;i++) dp[i][0]=1;
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=x;j++){
+            long long p=0;
+            if(j-coins[i]>=0){
+                p=(p+dp[i][j-coins[i]])%mod;
+            }
+            dp[i][j]=(p+dp[i+1][j])%mod;
+        }
+    }
+    cout<<dp[0][x]<<endl;
+    return 0;
+}
